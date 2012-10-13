dac0de0e3537cb1b3f417b17316d0b2098bd0c4e
Bryce Kerley <bkerley@brycekerley.net>
2012-10-13 19:34:42 -0400

    test show action correctly

--- test/functional/reviews_controller_test.rb
+++ test/functional/reviews_controller_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class ReviewsControllerTest < ActionController::TestCase
+  setup do
+    @answer = 'UPPER lower 123'
+
+    @user = User.create email: 'asdf@example.invalid', password: 'asdf'
+    @deck = CardDeck.create user_key: @user.key, name: 'test deck', description: 'asdf'
+    @card = Card.create card_deck_key: @deck.key, question: 'what', answer: @answer
+
+    @other_card = Card.create card_deck_key: @deck.key, question: 'ever', answer: 'ddd'
+  end
   test "should get show" do
-    get :show
+    get :show, id: @card.key
     assert_response :success
   end
 
