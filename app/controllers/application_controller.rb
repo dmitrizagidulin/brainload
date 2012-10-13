03abc00b0071a97131790ffc24c788fd906d5611
davidx <davidx@gmail.com>
2012-10-13 05:30:30 -0700

    temporary current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   def current_user
     # HACK is temporary
-    User.find_by_index(:email, "bob@example.com").first
+    User.find_by_index(:email, "bob@example.com").first || (@user=User.new(:email => "bob@example.com", :password => "231de2ewqa"); @user.save!)
   end
 end
