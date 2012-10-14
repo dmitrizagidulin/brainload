5f68e52a88e625d214d3a8894c5bfa50515f9261
Dmitri Zagidulin <dzagidulin@gmail.com>
2012-10-14 13:55:52 -0400

    Display public decks by category

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,13 @@ class WelcomeController < ApplicationController
       user = User.find_by_index(:email, session[:current_user]).first
       @card_decks = user ? user.card_decks : []
     end
-    @public_decks = CardDeck.public_decks
+    public_decks = CardDeck.public_decks
+    @public_decks_by_category = public_decks.group_by(&:category_key)
+    
+    # Get a sorted list of only categories with public decks
+    @public_categories = Category.all
+    @public_categories = @public_categories.reject { |c| c unless @public_decks_by_category.has_key? c.key }
+    @public_categories = @public_categories.sort_by { |c| c.name }
   end
 
   def home
