9f9c4ae6a03ecdb62cdbdc61d50de272f28c77ce
Dmitri Zagidulin <dzagidulin@gmail.com>
2012-10-13 20:38:15 -0400

    Add Category model, for decks

--- app/models/card_deck.rb
+++ app/models/card_deck.rb
@@ -4,10 +4,12 @@ class CardDeck
   property :name, String, presence: true, index: true
   property :description, String, presence: true, index: true
   property :user_key, String, presence: true, index: true
+	property :category_key, String, index: true
   timestamps!
 
   one :user, using: :stored_key
-
+  one :category, using: :stored_key
+  
   def cards
     Card.find_by_index(:card_deck_key, self.key)
   end
