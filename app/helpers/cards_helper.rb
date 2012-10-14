4459355198692aaa0317f3601f13ef7745ca3ba4
Bryce Kerley <bkerley@brycekerley.net>
2012-10-14 17:56:03 -0400

    Handle a nil for next sanely

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,7 +1,18 @@
 module CardsHelper
   def onward_button
+    next_key = review.next
+    return return_button if next_key.nil?
+
     link_to("\u23ce Onward!",
-            review_path(review.next),
+            review_path(next_key),
+            class: 'btn btn-large',
+            :'data-press-enter' => true
+      )
+  end
+
+  def return_button
+    link_to("\u23ce Return to Deck",
+            card_deck_path(deck.key),
             class: 'btn btn-large',
             :'data-press-enter' => true
       )
