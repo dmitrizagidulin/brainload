f42c092628d18201743ba228c09452962c512258
davidx <davidx@gmail.com>
2012-10-13 23:33:57 -0700

    add index

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,8 @@
 class Category
   include Ripple::Document
 
-  property :name, String, presence: true  # e.g. 'Computer Science'
-  property :url_slug, String, presence: true  # e.g. 'comp_sci' - used for human readable unique keys
+  property :name, String, presence: true, index: true  # e.g. 'Computer Science'
+  property :url_slug, String, presence: true, index: true  # e.g. 'comp_sci' - used for human readable unique keys
   timestamps!
 
   key_on :url_slug
