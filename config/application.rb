e9a0b1ca231b6424a76404b69949a5d5a52b7a8b
Bryce Kerley <bkerley@brycekerley.net>
2012-10-14 19:17:08 -0400

    fix Font Awesome configuration

--- config/application.rb
+++ config/application.rb
@@ -72,5 +72,7 @@ module R12Team241
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'font')
   end
 end
